09595e365dac2d6266ac8655a14647feb9067f26
阿瓦隆 <awalong@wacai.com>
2018-03-20 16:35:02 +0800

    使用axios

--- src/middlewares/fetch.js
+++ src/middlewares/fetch.js
@@ -1,13 +1,12 @@
+import axios from 'axios'
+
 export default function (url, originOptions) {
   const options = {
     credentials: 'include',
     ...originOptions
   }
 
-  return fetch(url, options).then(response => {
-    if (response.headers.get('content-type').includes('application/json')) {
-      return response.json()
-    }
-    return response.text()
+  return axios(url, options).then(response => {
+    return response.data
   })
 }
