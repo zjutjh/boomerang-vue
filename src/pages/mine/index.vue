e8ef0f86b0ed7759baf7751c685418a7700b1bcc
cccRaim <chenran@zjut.com>
2018-04-07 02:31:14 +0800

    我的页面css完成

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="container">
-    我的
+    <div class="m-user-info">
+      <div class="avatar">
+        <img src=""/>
+      </div>
+      <div class="content">
+        <div class="nickname">尼古拉斯</div>
+        <div class="sub-info">QQ：未填写</div>
+        <div class="sub-info">手机号：1578888888</div>
+      </div>
+    </div>
+
+    <div class="m-setting-list">
+      <div class="title">失物招领</div>
+      <ul class="list">
+        <li class="item touchable">
+          <div class="label">已寻回</div>
+          <div class="badge">6</div>
+        </li>
+        <li class="item touchable">
+          <div class="label">已认领</div>
+          <div class="badge">7</div>
+        </li>
+        <li class="item touchable">
+          <div class="label">未完成</div>
+          <div class="badge">0</div>
+        </li>
+      </ul>
+      <div class="title">设置</div>
+      <ul class="list">
+        <li class="item touchable">
+          <div class="label">更改联系方式</div>
+        </li>
+        <li class="item touchable">
+          <div class="label">关于</div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -21,4 +57,5 @@ export default {
 </script>
 
 <style lang="scss" rel="stylesheet/scss" type="text/css">
+  @import "./style.scss";
 </style>
