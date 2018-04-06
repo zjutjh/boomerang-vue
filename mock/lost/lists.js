91af8477f3eeb36f06ede980dd2bf28e07648a1e
cccRaim <chenran@zjut.com>
2018-04-07 00:18:26 +0800

    edit mock

--- mock/lost/lists.js
+++ mock/lost/lists.js
@@ -58,7 +58,223 @@ module.exports = function (req) {
           'contact_uno': '',
           'created_at': 'xxxx-xx-xx xx:xx:xx',
           'updated_at': 'xxxx-xx-xx xx:xx:xx'
-        }
+        },
+        {
+          'id': 123, // 物品id
+          'title': 'lostlostlostlostlostlostlostlostlost', // 物品名称
+          'description': '掉在洗衣房了，很着急，求好心人', // 描述
+          'lost_place': '西十五洗衣房', // 丢失地点
+          'images': [ // 图片
+            '//up.enterdesk.com/edpic_source/55/34/ac/5534acffb3f7e6d00de9f40176e68cd9.jpg'
+          ],
+          'lost_type': 0, // 失物
+          'type': { // 由type_id查询而来
+            'id': 1,
+            'name': '校园卡',
+            'icon': 'xxx'
+          },
+          'qq': 'xxxx',
+          'phone': 'xxxxx',
+          'status': 1, // 已寻回
+          'deleted': 0, // 未删除
+          'user': { // 发布者信息，由uid查询而来
+            'id': 1, // 用户id
+            'uno': 'xxxx' // 用户学号
+            // 其他字段看需求是否增加
+          },
+          'contact_uno': '',
+          'created_at': 'xxxx-xx-xx xx:xx:xx',
+          'updated_at': 'xxxx-xx-xx xx:xx:xx'
+        },
+        {
+          'id': 111, // 物品id
+          'title': '葫芦娃塑料桶',
+          'description': '葫芦娃塑料桶，是哪个人的？葫芦娃塑料桶，是哪个人的？葫芦娃塑料桶，是哪个人的？',
+          'lost_place': '西十五洗衣房',
+          'images': [
+            '//up.enterdesk.com/edpic_source/62/7a/6b/627a6ba1dd7bf82738c85bf6660d0a39.jpg'
+          ],
+          'lost_type': 1, // 招领
+          'type': { // 由type_id查询而来
+            'id': 4,
+            'name': '其他',
+            'icon': 'xxx'
+          },
+          'qq': 'xxxx',
+          'phone': 'xxxxx',
+          'status': 0, // 还在寻主中
+          'deleted': 0, // 未删除
+          'user': { // 发布者信息，由uid查询而来
+            'id': 2, // 用户id
+            'uno': 'xxxx' // 用户学号
+            // 其他字段看需求是否增加
+          },
+          'contact_uno': '',
+          'created_at': 'xxxx-xx-xx xx:xx:xx',
+          'updated_at': 'xxxx-xx-xx xx:xx:xx'
+        },
+        {
+          'id': 123, // 物品id
+          'title': 'lostlostlostlostlostlostlostlostlost', // 物品名称
+          'description': '掉在洗衣房了，很着急，求好心人', // 描述
+          'lost_place': '西十五洗衣房', // 丢失地点
+          'images': [ // 图片
+            '//up.enterdesk.com/edpic_source/55/34/ac/5534acffb3f7e6d00de9f40176e68cd9.jpg'
+          ],
+          'lost_type': 0, // 失物
+          'type': { // 由type_id查询而来
+            'id': 1,
+            'name': '校园卡',
+            'icon': 'xxx'
+          },
+          'qq': 'xxxx',
+          'phone': 'xxxxx',
+          'status': 1, // 已寻回
+          'deleted': 0, // 未删除
+          'user': { // 发布者信息，由uid查询而来
+            'id': 1, // 用户id
+            'uno': 'xxxx' // 用户学号
+            // 其他字段看需求是否增加
+          },
+          'contact_uno': '',
+          'created_at': 'xxxx-xx-xx xx:xx:xx',
+          'updated_at': 'xxxx-xx-xx xx:xx:xx'
+        },
+        {
+          'id': 111, // 物品id
+          'title': '葫芦娃塑料桶',
+          'description': '葫芦娃塑料桶，是哪个人的？葫芦娃塑料桶，是哪个人的？葫芦娃塑料桶，是哪个人的？',
+          'lost_place': '西十五洗衣房',
+          'images': [
+            '//up.enterdesk.com/edpic_source/62/7a/6b/627a6ba1dd7bf82738c85bf6660d0a39.jpg'
+          ],
+          'lost_type': 1, // 招领
+          'type': { // 由type_id查询而来
+            'id': 4,
+            'name': '其他',
+            'icon': 'xxx'
+          },
+          'qq': 'xxxx',
+          'phone': 'xxxxx',
+          'status': 0, // 还在寻主中
+          'deleted': 0, // 未删除
+          'user': { // 发布者信息，由uid查询而来
+            'id': 2, // 用户id
+            'uno': 'xxxx' // 用户学号
+            // 其他字段看需求是否增加
+          },
+          'contact_uno': '',
+          'created_at': 'xxxx-xx-xx xx:xx:xx',
+          'updated_at': 'xxxx-xx-xx xx:xx:xx'
+        },
+        {
+          'id': 123, // 物品id
+          'title': 'lostlostlostlostlostlostlostlostlost', // 物品名称
+          'description': '掉在洗衣房了，很着急，求好心人', // 描述
+          'lost_place': '西十五洗衣房', // 丢失地点
+          'images': [ // 图片
+            '//up.enterdesk.com/edpic_source/55/34/ac/5534acffb3f7e6d00de9f40176e68cd9.jpg'
+          ],
+          'lost_type': 0, // 失物
+          'type': { // 由type_id查询而来
+            'id': 1,
+            'name': '校园卡',
+            'icon': 'xxx'
+          },
+          'qq': 'xxxx',
+          'phone': 'xxxxx',
+          'status': 1, // 已寻回
+          'deleted': 0, // 未删除
+          'user': { // 发布者信息，由uid查询而来
+            'id': 1, // 用户id
+            'uno': 'xxxx' // 用户学号
+            // 其他字段看需求是否增加
+          },
+          'contact_uno': '',
+          'created_at': 'xxxx-xx-xx xx:xx:xx',
+          'updated_at': 'xxxx-xx-xx xx:xx:xx'
+        },
+        {
+          'id': 111, // 物品id
+          'title': '葫芦娃塑料桶',
+          'description': '葫芦娃塑料桶，是哪个人的？葫芦娃塑料桶，是哪个人的？葫芦娃塑料桶，是哪个人的？',
+          'lost_place': '西十五洗衣房',
+          'images': [
+            '//up.enterdesk.com/edpic_source/62/7a/6b/627a6ba1dd7bf82738c85bf6660d0a39.jpg'
+          ],
+          'lost_type': 1, // 招领
+          'type': { // 由type_id查询而来
+            'id': 4,
+            'name': '其他',
+            'icon': 'xxx'
+          },
+          'qq': 'xxxx',
+          'phone': 'xxxxx',
+          'status': 0, // 还在寻主中
+          'deleted': 0, // 未删除
+          'user': { // 发布者信息，由uid查询而来
+            'id': 2, // 用户id
+            'uno': 'xxxx' // 用户学号
+            // 其他字段看需求是否增加
+          },
+          'contact_uno': '',
+          'created_at': 'xxxx-xx-xx xx:xx:xx',
+          'updated_at': 'xxxx-xx-xx xx:xx:xx'
+        },
+        {
+          'id': 123, // 物品id
+          'title': 'lostlostlostlostlostlostlostlostlost', // 物品名称
+          'description': '掉在洗衣房了，很着急，求好心人', // 描述
+          'lost_place': '西十五洗衣房', // 丢失地点
+          'images': [ // 图片
+            '//up.enterdesk.com/edpic_source/55/34/ac/5534acffb3f7e6d00de9f40176e68cd9.jpg'
+          ],
+          'lost_type': 0, // 失物
+          'type': { // 由type_id查询而来
+            'id': 1,
+            'name': '校园卡',
+            'icon': 'xxx'
+          },
+          'qq': 'xxxx',
+          'phone': 'xxxxx',
+          'status': 1, // 已寻回
+          'deleted': 0, // 未删除
+          'user': { // 发布者信息，由uid查询而来
+            'id': 1, // 用户id
+            'uno': 'xxxx' // 用户学号
+            // 其他字段看需求是否增加
+          },
+          'contact_uno': '',
+          'created_at': 'xxxx-xx-xx xx:xx:xx',
+          'updated_at': 'xxxx-xx-xx xx:xx:xx'
+        },
+        {
+          'id': 111, // 物品id
+          'title': '葫芦娃塑料桶',
+          'description': '葫芦娃塑料桶，是哪个人的？葫芦娃塑料桶，是哪个人的？葫芦娃塑料桶，是哪个人的？',
+          'lost_place': '西十五洗衣房',
+          'images': [
+            '//up.enterdesk.com/edpic_source/62/7a/6b/627a6ba1dd7bf82738c85bf6660d0a39.jpg'
+          ],
+          'lost_type': 1, // 招领
+          'type': { // 由type_id查询而来
+            'id': 4,
+            'name': '其他',
+            'icon': 'xxx'
+          },
+          'qq': 'xxxx',
+          'phone': 'xxxxx',
+          'status': 0, // 还在寻主中
+          'deleted': 0, // 未删除
+          'user': { // 发布者信息，由uid查询而来
+            'id': 2, // 用户id
+            'uno': 'xxxx' // 用户学号
+            // 其他字段看需求是否增加
+          },
+          'contact_uno': '',
+          'created_at': 'xxxx-xx-xx xx:xx:xx',
+          'updated_at': 'xxxx-xx-xx xx:xx:xx'
+        },
       ]
     }
   }
