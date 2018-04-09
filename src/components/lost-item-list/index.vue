ba222490a2fdfa3aa519bd2f79296e362d745cde
cccRaim <chenran@zjut.com>
2018-04-09 19:36:05 +0800

    首页item增加标签

--- src/components/lost-item-list/index.vue
+++ src/components/lost-item-list/index.vue
@@ -9,9 +9,9 @@
     <ul class="list" v-else>
       <router-link tag="li" :to="'/detail/' + item.id" class="item" v-for="(item, index) in source" :key="'lost-item-list_' + type + '_' + index">
         <div class="content">
-          <div class="title">{{item.title}}</div>
+          <div class="title"><span class="badge">{{ type === 'lost' ? '失物' : '招领'}}</span>{{item.title}}</div>
           <div class="description">{{item.description}}</div>
-          <div class="subtitle">{{item.type.name}} · {{item.updated_at}}</div>
+          <div class="subtitle"><span class="badge">{{item.type.name}}</span>{{item.updated_at}}</div>
         </div>
         <div class="image" :style="{
           backgroundImage: item.images[0] ? `url(${item.images[0]})` : '',
