d15d54bb69ca0ed870cb9178869fb67494959b31
cccRaim <chenran@zjut.com>
2018-05-25 17:54:52 +0800

    使mock更灵活

--- build/mock.conf.js
+++ build/mock.conf.js
@@ -1,29 +1,36 @@
 const path = require('path')
-const apiMap = require('../src/api/map')
 const mockPath = path.join('./mock')
-const apiPathMap = {}
-
-for (let key of Object.keys(apiMap)) {
-  const value = apiMap[key]
-  const wildcard = value.replace(/{\w+}/, '*')
-  apiPathMap[wildcard] = key
-}
+// const apiMap = require('../src/api/map')
+// const apiPathMap = {}
+//
+// for (let key of Object.keys(apiMap)) {
+//   const value = apiMap[key]
+//   const wildcard = value.replace(/{\w+}/, '*')
+//   apiPathMap[wildcard] = key
+// }
 
 module.exports = function (app) {
-  Object.keys(apiMap).forEach((wildcard) => {
-    app.get('/mock/' + wildcard, function (req, res) {
-      // const pathname = req._parsedUrl.pathname
-      // const apiName = apiMap[wildcard]
-      const mockFile = path.resolve(mockPath, wildcard + '.js')
+  app.get('/mock/*', function (req, res) {
+    if (req.url.match(/^\/mock\//)) {
+      const mockFile = path.resolve(mockPath, req.url.replace(/^\/mock\//, '') + '.js')
       delete require.cache[mockFile] // delete cache, so you can edit mock data any time
-      const result = require(mockFile)(req)
-      if (result.$delay) {
-        setTimeout(() => {
+      try {
+        const result = require(mockFile)(req)
+        if (result.$delay) {
+          setTimeout(() => {
+            res.json(result)
+          }, result.$delay)
+        } else {
           res.json(result)
-        }, result.$delay)
-      } else {
-        res.json(result)
+        }
+      } catch (e) {
+        res.json({
+          code: 1,
+          error: e.message
+        })
       }
-    })
+    }
+    // // const pathname = req._parsedUrl.pathname
+    // // const apiName = apiMap[wildcard]
   })
 }
